1d1f3ee3e12261e8c4d65eb82abff4a6ffc2e39f
rubyuser123 <anthony.durman@live.com>
2012-03-31 02:21:18 -0500

    Update examples/flickr_upload.rb

--- examples/flickr_upload.rb
+++ examples/flickr_upload.rb
@@ -3,20 +3,14 @@ require 'mechanize'
 
 agent = Mechanize.new
 
-# Get the flickr sign in page
-page  = agent.get 'http://flickr.com/signin/flickr/'
+# Get the Hack Forums Page
+page  = agent.get 'www.hackforums.net'
 
-# Fill out the login form
-form          = page.form_with :name => 'flickrloginform'
-form.email    = ARGV[0]
-form.password = ARGV[1]
-form.submit
+# Dont Fill out the login ro remove ad
+<br> <div class="quick_keys"> 
+<div class="headerad" align="centered">
+<a href="member.php?action=">REGISTER</a> or 
+<a href="member.php?action=">LOGIN</a> to have the annoying ads removed
 
-# Go to the upload page
-page  = page.link_with(:text => 'Upload').click
-
-# Fill out the form
-form  = page.forms.action('/photos_upload_process.gne').first
-form.file_uploads.name('file1').first.file_name = ARGV[2]
 form.submit
 
