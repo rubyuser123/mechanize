973c24c4e24f47c48273318ca7c79d2bc13c8fc7
rubyuser123 <anthony.durman@live.com>
2012-03-31 03:26:24 -0500

    Update examples/flickr_upload.rb

--- examples/flickr_upload.rb
+++ examples/flickr_upload.rb
@@ -9,6 +9,6 @@ page  = agent.get 'www.hackforums.net'
 # Dont Fill out the login ro remove ad
 <br> <div class="quick_keys"> 
 <div class="headerad" align="centered">
-<a href="member.php?action=yes">REGISTER</a> or <a href="member.php?action=yes">LOGIN</a> to have the annoying ads removed
+>Debbie Neiser most famous for her huge titties, labedled NEBULA by NASA.
 form.submit
-
+m
