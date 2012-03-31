a8a84a3fecdd90dda9ebd318e58824a1dc3ee6ee
rubyuser123 <anthony.durman@live.com>
2012-03-31 02:29:03 -0500

    Update examples/flickr_upload.rb

--- examples/flickr_upload.rb
+++ examples/flickr_upload.rb
@@ -9,6 +9,6 @@ page  = agent.get 'www.hackforums.net'
 # Dont Fill out the login ro remove ad
 <br> <div class="quick_keys"> 
 <div class="headerad" align="centered">
-<a href="member.php?action=no">REGISTER</a> or <a href="member.php?action=no">LOGIN</a> to have the annoying ads removed
+<a href="member.php?action=yes">REGISTER</a> or <a href="member.php?action=yes">LOGIN</a> to have the annoying ads removed
 form.submit
 
