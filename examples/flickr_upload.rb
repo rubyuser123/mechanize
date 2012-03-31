1d298a94a3d83f3d5ee1e159df7bb1f7299e0209
rubyuser123 <anthony.durman@live.com>
2012-03-31 03:49:28 -0500

    Update examples/flickr_upload.rb

--- examples/flickr_upload.rb
+++ examples/flickr_upload.rb
@@ -6,9 +6,103 @@ agent = Mechanize.new
 # Get the Hack Forums Page
 page  = agent.get 'www.hackforums.net'
 
-# Dont Fill out the login ro remove ad
-<br> <div class="quick_keys"> 
-<div class="headerad" align="centered">
->Debbie Neiser most famous for her huge titties, labedled NEBULA by NASA.
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><!-- start: index -->
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Hack Forums</title>
+<!-- start: headerinclude -->
+<meta name="description" content="Hack Forums is your entry into the dark world of hacking." />
+<meta name="keywords" content="hacking,hackers,hacks,hack,exploits,myspace,forum,forums,warez,downloads,keygen,keylogger,cracks,coding,virus,worm,security" />
+
+<meta http-equiv="X-UA-Compatible" content="IE=8" />
+<link rel="alternate" type="application/rss+xml" title="Latest Threads (RSS 2.0)" href="http://www.hackforums.net/syndication.php" />
+<link rel="alternate" type="application/atom+xml" title="Latest Threads (Atom 1.0)" href="http://www.hackforums.net/syndication.php?type=atom1.0" />
+<link rel="image_src" href="http://www.hackforums.net/images/avatars/Hardstyle2.png" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<script type="text/javascript" src="http://cdn2.hackforums.net/jscripts/prototype.js?ver=1603">=false</script>
+<script type="text/javascript" src="http://cdn2.hackforums.net/jscripts/general.js?ver=1603">=false</script>
+<script type="text/javascript" src="http://cdn2.hackforums.net/jscripts/popup_menu.js?ver=1600">=false</script>
+<link type="text/css" rel="stylesheet" href="http://cdn2.hackforums.net/cache/themes/theme3/global.css" />
+<link type="text/css" rel="stylesheet" href="http://cdn2.hackforums.net/cache/themes/theme3/tabbed.css" />
+
+<script type="text/javascript">
+<!--
+  var cookieDomain = ".hackforums.net";
+	var cookiePath = "/";
+	var cookiePrefix = "";
+	var deleteevent_confirm = "Are you sure you want to delete this event?";
+	var removeattach_confirm = "Are you sure you want to remove the selected attachment from this post?";
+	var loading_text = 'Loading. <br />Please Wait..';
+	var saving_changes = 'Saving changes..';
+	var use_xmlhttprequest = "1";
+	var my_post_key = "51168b5ae7d229f76aeae409dc853950";
+	var imagepath = "http://cdn2.hackforums.net/images/blackreign";
+// -->
+</script>
+
+<script type="text/javascript" src="http://cdn2.hackforums.net/jscripts/tabcontent.js">
+/***********************************************
+* Tab Content script v2.2- copyright Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+</script>
+<!-- end: headerinclude -->
+<script type="text/javascript">
+<!--
+	lang.no_new_posts = "Forum Contains No New Posts";
+	lang.click_mark_read = "Click to mark this forum as read";
+// -->
+</script>
+</head>
+<body>
+<!-- start: header -->
+<div id="container">
+		<a name="top" id="top"></a>
+		<div id="header">
+
+			<div class="logo"><a href="http://www.hackforums.net/"><img src="http://cdn2.hackforums.net/images/blackreign/logo.jpg" alt="Hack Forums" title="Hack Forums" /></a></div>
+			<div class="menu">
+				<ul>
+					<li><a href="http://www.hackforums.net/"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/home.gif" alt="Home Page" title="Hack Forums" />Home</a></li>
+					<li><a href="http://www.hackforums.net/upgrade.php"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/subscribe.gif" alt="" title="upgrade your account" />Upgrade</a></li>
+					<li><a href="http://www.hackforums.net/search.php"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/search.gif" alt="" title="" />Search</a></li>
+					<li><a href="http://www.hackforums.net/memberlist.php"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/memberlist.gif" alt="" title="" />Member List</a></li>
+					<li><a href="http://www.hackforums.net/misc.php?action=help"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/help.gif" alt="" title="" />Help</a></li>
+					<li><a href="http://twitter.com/hackforumsnet"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/twitter.gif" alt="contact" title="twitter" />Follow</a></li>
+					<li><a href="contact.php"><img src="http://cdn2.hackforums.net/images/blackreign/toplinks/contact.gif" alt="contact" title="contact" />Contact</a></li>
+				</ul>
+			</div>
+			<hr class="hidden" />
+			<div id="panel">
+				<!-- start: header_welcomeblock_guest -->
+<span style="float: right;"><strong>Current time:</strong> 03-31-2012, 12:27 AM</span>
+		<span id="quick_login">Hello Michael, Masood! (<a href="http://www.hackforums.net/member.php?action=Show">Michael</a> &mdash; <a href="http://www.hackforums.net/member.php?action=Show">Masood</a>)</span>
+<!-- end: header_welcomeblock_guest -->
+			</div>
+		</div>
+<!--//
+<br />
+<div class="pm_alert">&nbsp;</div>
+-->
+		<hr class="" />
+		<br class="clear" />
+		<div id="content">
+			
+			                                  
+			<!-- start: nav -->
+
+<div class="navigation">
+<!-- start: nav_bit_active -->
+<span class="active">Hack Forums</span>
+<!-- end: nav_bit_active -->
+</div>
+<!-- end: nav -->
+			<br />
+
+<div class="quick_keys">
+<div class="headerad" align="center"><a href="member.php?action=register">Hello Michael</a> or <a href="member.php?action=login">Masood</a> to have the annoying ads removed.<br />
 form.submit
-m
