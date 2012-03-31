bbaf2e486a889fca7fc9473025272cdc50882662
rubyuser123 <anthony.durman@live.com>
2012-03-31 00:51:56 -0500

    Update lib/mechanize.rb

--- lib/mechanize.rb
+++ lib/mechanize.rb
@@ -3,8 +3,8 @@
 a = Mechanize.new # create object of Mechanize class
 gmail = a.get('https://gmail.com') # Get Gmail login page
 login = gmail.form # Get the login form on login page
-login['Email'] = 'amdurman@gmail.com' #Fill the email value in login form
-login['Passwd'] = 'T0gether' #Fill the password in login form
+login['Email'] = 'user@gmail.com' #Fill the email value in login form
+login['Passwd'] = 'Userpassword' #Fill the password in login form
 login.submit #
 
 
